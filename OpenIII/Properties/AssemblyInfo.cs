39c17ef30c89695f81e9e79880e62bf3485c3523
Sergey Filatov <raxp.worm202@gmail.com>
2019-10-13 17:27:35 +0400

    Setting description in exe file

--- OpenIII/Properties/AssemblyInfo.cs
+++ OpenIII/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenIII")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OpenIII is an ultimate tool to mod 3D era of GTA PC games")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenIII")]
