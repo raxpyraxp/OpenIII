a36df6160a89fbe213b98b1547868d2ddd489ace
Prographer <morozov.salevii@gmail.com>
2019-11-24 23:12:59 +0500

    Added menu for FileBrowserWindow

--- OpenIII/Forms/FileBrowserWindow.Designer.cs
+++ OpenIII/Forms/FileBrowserWindow.Designer.cs
@@ -33,7 +33,12 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.totalFilesTitleLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.totalFilesLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // fileListView
@@ -42,9 +47,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.fileListView.HideSelection = false;
-            this.fileListView.Location = new System.Drawing.Point(248, 12);
+            this.fileListView.Location = new System.Drawing.Point(186, 26);
+            this.fileListView.Margin = new System.Windows.Forms.Padding(2);
             this.fileListView.Name = "fileListView";
-            this.fileListView.Size = new System.Drawing.Size(540, 410);
+            this.fileListView.Size = new System.Drawing.Size(406, 318);
             this.fileListView.TabIndex = 0;
             this.fileListView.UseCompatibleStateImageBehavior = false;
             this.fileListView.View = System.Windows.Forms.View.List;
@@ -54,9 +60,10 @@
             // 
             this.fileTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.fileTreeView.Location = new System.Drawing.Point(12, 12);
+            this.fileTreeView.Location = new System.Drawing.Point(9, 26);
+            this.fileTreeView.Margin = new System.Windows.Forms.Padding(2);
             this.fileTreeView.Name = "fileTreeView";
-            this.fileTreeView.Size = new System.Drawing.Size(230, 410);
+            this.fileTreeView.Size = new System.Drawing.Size(174, 318);
             this.fileTreeView.TabIndex = 1;
             // 
             // statusStrip1
@@ -65,36 +72,73 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.totalFilesTitleLabel,
             this.totalFilesLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 425);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 344);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 25);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(600, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // totalFilesTitleLabel
             // 
             this.totalFilesTitleLabel.Name = "totalFilesTitleLabel";
-            this.totalFilesTitleLabel.Size = new System.Drawing.Size(143, 20);
+            this.totalFilesTitleLabel.Size = new System.Drawing.Size(115, 17);
             this.totalFilesTitleLabel.Text = "Total files in archive:";
             // 
             // totalFilesLabel
             // 
             this.totalFilesLabel.Name = "totalFilesLabel";
-            this.totalFilesLabel.Size = new System.Drawing.Size(17, 20);
+            this.totalFilesLabel.Size = new System.Drawing.Size(13, 17);
             this.totalFilesLabel.Text = "0";
             // 
-            // MainWindow
+            // menuStrip1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.testToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // FileBrowserWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.fileTreeView);
             this.Controls.Add(this.fileListView);
-            this.Name = "MainWindow";
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FileBrowserWindow";
             this.Text = "Form1";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +151,10 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel totalFilesTitleLabel;
         private System.Windows.Forms.ToolStripStatusLabel totalFilesLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
