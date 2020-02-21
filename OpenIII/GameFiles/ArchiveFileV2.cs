fe43f96318fcf53beb9adc36760cac55e1d59888
Sergey Filatov <raxp.worm202@gmail.com>
2020-02-21 17:13:50 +0400

    Archive writing version 2 implementation

--- OpenIII/GameFiles/ArchiveFileV2.cs
+++ OpenIII/GameFiles/ArchiveFileV2.cs
@@ -108,7 +108,7 @@ namespace OpenIII.GameFiles
             return fileList;
         }
 
-        public int GetFirstFileOffset()
+        public GameFile GetFirstFile()
         {
             GameFile firstFile = null;
 
@@ -120,17 +120,167 @@ namespace OpenIII.GameFiles
                 }
             }
 
-            return firstFile.Offset;
+            return firstFile;
+        }
+
+        public int GetFirstFileOffset()
+        {
+            return GetFirstFile().Offset;
+        }
+
+        public byte[] CreateNewEntry(int offset, long length, string filename)
+        {
+            MemoryStream stream = new MemoryStream();
+            byte[] buf;
+            byte[] entry = new byte[DIR_ENTRY_SIZE];
+
+            buf = new byte[OFFSET_ENTRY_BYTE_SIZE];
+            buf = BitConverter.GetBytes((int)Math.Ceiling((double)offset / SECTOR_SIZE));
+            stream.Write(buf, 0, buf.Length);
+
+            buf = new byte[STREAMING_ENTRY_BYTE_SIZE];
+            buf = BitConverter.GetBytes((ushort)Math.Ceiling((double)length / SECTOR_SIZE));
+            stream.Write(buf, 0, buf.Length);
+
+            // It was never used in production game release, so we just write it with zeroes
+            buf = new byte[SIZE_ENTRY_BYTE_SIZE];
+            stream.Write(buf, 0, buf.Length);
+
+            buf = new byte[FILENAME_ENTRY_BYTE_SIZE];
+            Encoding.ASCII.GetBytes(filename + "\0").CopyTo(buf, 0);
+            stream.Write(buf, 0, FILENAME_ENTRY_BYTE_SIZE);
+
+            stream.Flush();
+            stream.ToArray().CopyTo(entry, 0);
+            stream.Close();
+            return entry;
+        }
+
+        public byte[] CreateHeader(int numberOfFiles)
+        {
+            MemoryStream stream = new MemoryStream();
+            byte[] buf;
+            byte[] header = new byte[HEADER_SIZE];
+
+            // Writing version
+            buf = new byte[VERSION_SIZE];
+            Encoding.ASCII.GetBytes("VER2").CopyTo(buf, 0);
+            stream.Write(buf, 0, buf.Length);
+
+            // Copy and modify total files
+            buf = new byte[NUMBER_OF_ENTRIES_SIZE];
+            BitConverter.GetBytes(numberOfFiles).CopyTo(buf, 0);
+            stream.Write(buf, 0, NUMBER_OF_ENTRIES_SIZE);
+
+            stream.Flush();
+            stream.ToArray().CopyTo(header, 0);
+            stream.Close();
+            return header;
+        }
+
+        public byte[] CreateFileTableSection(List<GameFile> entries)
+        {
+            MemoryStream tempStream = new MemoryStream();
+            byte[] fileTableSection;
+            byte[] header;
+            byte[] newEntry;
+
+            // Create archive version header
+            header = CreateHeader(entries.Count);
+            tempStream.Write(header, 0, header.Length);
+
+            // Copy file entries
+            foreach (GameFile entry in entries)
+            {
+                newEntry = CreateNewEntry(entry.Offset, entry.Length, entry.Name);
+                tempStream.Write(newEntry, 0, newEntry.Length);
+            }
+
+            tempStream.Flush();
+            fileTableSection = tempStream.ToArray();
+            tempStream.Close();
+            return fileTableSection;
         }
 
         public override void AddNewFileEntry(int offset, GameFile file)
         {
-            throw new NotImplementedException();
+            List<GameFile> entries = new List<GameFile>();
+            int firstFileOffset = GetFirstFileOffset();
+            byte[] buf;
+            byte[] fileTableSection;
+            int read = 1;
+
+            // TODO: We should find better solution for this mess
+            foreach (GameFile entry in GetFileList())
+            {
+                entries.Add(entry);
+            }
+
+            file.PrepareForArchiving(offset);
+            entries.Add(file);
+
+            fileTableSection = CreateFileTableSection(entries);
+
+            // Check if new IMG header doesn't overlap the first file
+            if (fileTableSection.Length > firstFileOffset)
+            {
+                // We should correctly remove it and write it to the end
+                GameFile firstFile = GetFirstFile();
+                GameFile tmpFile = new GameFile(Path.GetTempFileName());
+                firstFile.Extract(tmpFile.FullPath);
+                firstFile.Delete();
+                InsertFile(tmpFile);
+                tmpFile.Delete();
+
+                // Try to add file entry again
+                AddNewFileEntry(offset, file);
+            }
+            else
+            {
+                MemoryStream headerStream = new MemoryStream(fileTableSection);
+                Stream archiveStream = GetStream(FileMode.Open, FileAccess.ReadWrite);
+
+                while (headerStream.Position < headerStream.Length)
+                {
+                    buf = new byte[SECTOR_SIZE];
+                    read = headerStream.Read(buf, 0, SECTOR_SIZE);
+                    archiveStream.Write(buf, 0, read);
+                }
+
+                headerStream.Close();
+                archiveStream.Close();
+            }
         }
 
         public override void DeleteFileEntry(GameFile file)
         {
-            throw new NotImplementedException();
+            List<GameFile> entries = new List<GameFile>();
+            byte[] buf;
+            byte[] fileTableSection;
+            int read = 1;
+
+            // TODO: We should find better solution for this mess
+            foreach (GameFile entry in GetFileList())
+            {
+                if (!file.Equals(entry))
+                {
+                    entries.Add(entry);
+                }
+            }
+
+            fileTableSection = CreateFileTableSection(entries);
+            MemoryStream headerStream = new MemoryStream(fileTableSection);
+            Stream archiveStream = GetStream(FileMode.Open, FileAccess.ReadWrite);
+
+            while (headerStream.Position < headerStream.Length)
+            {
+                buf = new byte[SECTOR_SIZE];
+                read = headerStream.Read(buf, 0, SECTOR_SIZE);
+                archiveStream.Write(buf, 0, read);
+            }
+
+            headerStream.Close();
+            archiveStream.Close();
         }
     }
 }
