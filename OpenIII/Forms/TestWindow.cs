631bda9a0c5c4c1ffb9083d9cb77291df3ae24b9
Prographer <morozov.salevii@gmail.com>
2020-10-29 00:20:19 +0500

    Added TestWindow for testing config parsing

---
+++ OpenIII/Forms/TestWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenIII.Forms
+{
+    public partial class TestWindow : OpenIII.Forms.BaseWindow
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
