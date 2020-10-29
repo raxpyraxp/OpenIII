f3fbf2ee25b6fea31569a5b6a46c6865685cd6d5
Prographer <morozov.salevii@gmail.com>
2020-10-30 00:03:45 +0500

    Included IPLSections in project and added TextBox in TestWindow

--- OpenIII/Forms/TestWindow.Designer.cs
+++ OpenIII/Forms/TestWindow.Designer.cs
@@ -28,9 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 418);
+            this.textBox1.TabIndex = 0;
+            // 
+            // TestWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.textBox1);
+            this.Name = "TestWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
