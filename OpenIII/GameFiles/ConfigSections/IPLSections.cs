4f386636d9e90155a3139c72dc3ec2a3aa8f5f37
Prographer <morozov.salevii@gmail.com>
2021-03-20 02:07:09 +0500

    Added IPL sections' classes

--- OpenIII/GameFiles/ConfigSections/IPLSections.cs
+++ OpenIII/GameFiles/ConfigSections/IPLSections.cs
@@ -135,4 +135,133 @@
             this.Lod = lod;
         }
     }
+
+
+    public class AUZO : ConfigRow
+    {
+        public const string SectionName = "auzo";
+    }
+
+    public class AUZOType1 : AUZO
+    {
+        private string Name { get; set; }
+
+        private int Id { get; set; }
+
+        private int Switch { get; set; }
+
+        private double X1 { get; set; }
+
+        private double Y1 { get; set; }
+
+        private double Z1 { get; set; }
+
+        private double X2 { get; set; }
+
+        private double Y2 { get; set; }
+
+        private double Z2 { get; set; }
+    }
+
+    public class AUZOType2 : AUZO
+    {
+        private string Name { get; set; }
+
+        private int Id { get; set; }
+
+        private int Switch { get; set; }
+
+        private double X1 { get; set; }
+
+        private double Y1 { get; set; }
+
+        private double Z1 { get; set; }
+
+        private double Radios { get; set; }
+    }
+
+
+    public class CULL : ConfigRow
+    {
+        public const string SectionName = "cull";
+    }
+
+    public class CULLType1 : CULL
+    {
+        private double CenterX { get; set; }
+
+        private double CenterY { get; set; }
+
+        private double CenterZ { get; set; }
+
+        private double X1 { get; set; }
+
+        private double Y1 { get; set; }
+
+        private double Z1 { get; set; }
+
+        private double X2 { get; set; }
+
+        private double Y2 { get; set; }
+
+        private double Z2 { get; set; }
+
+        private int Attribute { get; set; }
+
+        private int WantedLevelDrop { get; set; }
+    }
+
+    public class CULLType2 : CULL
+    {
+        private double CenterX { get; set; }
+
+        private double CenterY { get; set; }
+
+        private double CenterZ { get; set; }
+
+        private int Unknown1 { get; set; }
+
+        private double Lenght { get; set; }
+
+        private double Bottom { get; set; }
+
+        private double Width { get; set; }
+
+        private int Unknown2 { get; set; }
+
+        private double Top { get; set; }
+
+        private int Flag { get; set; }
+
+        private int Unknown3 { get; set; }
+    }
+
+    public class CULLType3 : CULL
+    {
+        private double CenterX { get; set; }
+
+        private double CenterY { get; set; }
+
+        private double CenterZ { get; set; }
+
+        private int Unknown1 { get; set; }
+
+        private double Lenght { get; set; }
+
+        private double Bottom { get; set; }
+
+        private double Width { get; set; }
+
+        private int Unknown2 { get; set; }
+
+        private double Top { get; set; }
+
+        private double Vx { get; set; }
+
+        private double Vy { get; set; }
+
+        private double Vz { get; set; }
+
+        private double Cm { get; set; }
+    }
 }
